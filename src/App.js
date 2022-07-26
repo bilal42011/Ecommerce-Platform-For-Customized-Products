d287db4b1b252531aca765383859d6e595e46df3
Muhammad Safwan Karim <msafwankarim786@gmail.com>
2022-07-26 15:25:33 +0500

    init

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React from "react";
 import Layout from "./Layout/Layout";
 import { Outlet } from "react-router-dom";
 
-
-const App=()=>{
-return (
-<Layout>
-    <Outlet/>
-</Layout>
-)
-}
+const App = () => {
+  return (
+    <Layout>
+      <Outlet />
+    </Layout>
+  );
+};
 
 export default App;
--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -2,12 +2,14 @@ import React from "react";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 
-const Layout=({children})=>{
-return <>
-<Header/>
-{children}
-<Footer/>
-</>
-}
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+};
 
 export default Layout;
