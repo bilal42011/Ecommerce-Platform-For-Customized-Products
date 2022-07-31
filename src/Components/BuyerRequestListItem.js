f0f0a958a5535a294d8bd46c81a9e58ec86744f4
Muhammad Safwan Karim <msafwankarim786@gmail.com>
2022-07-31 18:35:20 +0500

    view buyer request page completed

--- src/Components/BuyerRequestListItem.js
+++ src/Components/BuyerRequestListItem.js
@@ -1,53 +1,69 @@
 import {
   Avatar,
+  Button,
+  Divider,
   ListItem,
   ListItemAvatar,
-  ListItemButton,
   ListItemText,
   Paper,
   Stack,
   Typography,
 } from "@mui/material";
+import DownloadIcon from "@mui/icons-material/Download";
 import { Box } from "@mui/system";
 
 export default function BuyerRequestListItem({ request }) {
   return (
-    <Paper width="100%" variant="outlined">
-      <ListItem width="100%">
-        <ListItemAvatar>
-          <Avatar src={request.buyer_avatar}>
-            {request.buyer_full_name.charAt(0)}
-          </Avatar>
-        </ListItemAvatar>
-        <Stack direction="row" alignItems={"center"} width="100%">
-          <ListItemText
-            primary={request.buyer_username}
-            secondary={
-              <Typography variant="body2">
-                {request.buyer_city}, Pakistan
-              </Typography>
-            }
-          />
-          <Box flexGrow={1}></Box>
-          <Typography>
-            Budget:{"  "}
-            <Typography display="inline" fontWeight="bold">
-              Rs. {request.budget.toLocaleString()} /-
+    <ListItem width="100%">
+      <Paper width="100%" variant="outlined" sx={{ flex: 1, p: 1 }}>
+        <Stack spacing={1} width="100%">
+          <Stack direction="row" alignItems={"center"} width="100%">
+            <ListItemAvatar>
+              <Avatar src={request.buyer_avatar}>
+                {request.buyer_full_name.charAt(0)}
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText
+              primary={request.buyer_username}
+              secondary={
+                <Typography variant="body2">
+                  {request.buyer_city}, Pakistan
+                </Typography>
+              }
+            />
+            <Box flexGrow={1}></Box>
+            <Typography>
+              Budget:{"  "}
+              <strong>Rs. {request.budget.toLocaleString()} /-</strong>
             </Typography>
-          </Typography>
-          <Typography ml={2}>
-            Timeframe:{"  "}
-            <Typography fontWeight="bold" display="inline">
-              {request.time} Days
+            <Typography ml={2}>
+              Timeframe:{"  "}
+              <strong>{request.time} Days</strong>
             </Typography>
-          </Typography>
+          </Stack>
+          <Box mt={2}>
+            {" "}
+            {/*description */}
+            <Typography color="GrayText">{request.description}</Typography>
+          </Box>
+          <Divider />
+          <Stack direction="row" spacing={1} alignItems="center">
+            <Button variant="outlined">Contact Buyer</Button>
+            <Button variant="contained">Send Proposal</Button>
+            <Box flexGrow={1}></Box>
+            <Box className="attachments">
+              {request.attachments.map((elem, index) => {
+                return (
+                  <Button size="small" key={index} endIcon={<DownloadIcon />}>
+                    {/* TODO: Add file link */}
+                    {elem}
+                  </Button>
+                );
+              })}
+            </Box>
+          </Stack>
         </Stack>
-        <Box mt={2}>
-          {" "}
-          {/*description */}
-          {/* <Typography>{request.description}</Typography> */}
-        </Box>
-      </ListItem>
-    </Paper>
+      </Paper>
+    </ListItem>
   );
 }
--- src/Pages/ViewBuyerRequests.js
+++ src/Pages/ViewBuyerRequests.js
@@ -1,8 +1,10 @@
-import { List, Typography } from "@mui/material";
+import { Box, List, Pagination, Typography } from "@mui/material";
 import { Container } from "@mui/system";
+import { useState } from "react";
 import BuyerRequestListItem from "../Components/BuyerRequestListItem";
 
 export default function ViewBuyerRequests() {
+  const [page, setPage] = useState(1);
   const buyerRequests = [
     {
       buyer_avatar: "",
@@ -13,8 +15,69 @@ export default function ViewBuyerRequests() {
       budget: 7500,
       time: 12, //days
       description: "We are looking for a....",
+      attachments: ["file1.jpg", "file2.docx"],
+    },
+    {
+      buyer_avatar: "",
+      buyer_full_name: "User 3849",
+      buyer_id: "",
+      buyer_username: "user3849",
+      buyer_city: "Islamabad",
+      budget: 7500,
+      time: 12, //days
+      description: "We are looking for a....",
+      attachments: ["file1.jpg", "file2.docx"],
+    },
+    {
+      buyer_avatar: "",
+      buyer_full_name: "User 3849",
+      buyer_id: "",
+      buyer_username: "user3849",
+      buyer_city: "Islamabad",
+      budget: 7500,
+      time: 12, //days
+      description: "We are looking for a....",
+      attachments: ["file1.jpg", "file2.docx"],
+    },
+    {
+      buyer_avatar: "",
+      buyer_full_name: "User 3849",
+      buyer_id: "",
+      buyer_username: "user3849",
+      buyer_city: "Islamabad",
+      budget: 7500,
+      time: 12, //days
+      description: "We are looking for a....",
+      attachments: ["file1.jpg", "file2.docx"],
+    },
+    {
+      buyer_avatar: "",
+      buyer_full_name: "User 3849",
+      buyer_id: "",
+      buyer_username: "user3849",
+      buyer_city: "Islamabad",
+      budget: 7500,
+      time: 12, //days
+      description: "We are looking for a....",
+      attachments: ["file1.jpg", "file2.docx"],
+    },
+    {
+      buyer_avatar: "",
+      buyer_full_name: "User 3849",
+      buyer_id: "",
+      buyer_username: "user3849",
+      buyer_city: "Islamabad",
+      budget: 7500,
+      time: 12, //days
+      description: "We are looking for a....",
+      attachments: ["file1.jpg", "file2.docx"],
     },
   ];
+
+  const onPageChange = (event, newPage) => {
+    setPage(newPage);
+  };
+
   return (
     <Container maxWidth="xl" sx={{ mt: 15 }}>
       <Typography textAlign={"center"} variant="h4" component="h1">
@@ -25,6 +88,9 @@ export default function ViewBuyerRequests() {
           <BuyerRequestListItem request={item} key={index} />
         ))}
       </List>
+      <Box display="flex" justifyContent={"center"} mt={2}>
+        <Pagination count={10} page={page} onChange={onPageChange} />
+      </Box>
     </Container>
   );
 }
