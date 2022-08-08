cd9d41f0bbc43cac3f325a422db1be4915415e72
Bilal Malik <bilalmalik42011@gmail.com>
2022-08-09 03:43:04 +0500

    AsyncThunk in redux toolkit added and playground for it established in homepage.js page

--- src/Store/Slices/authSlice/authSlice.js
+++ src/Store/Slices/authSlice/authSlice.js
@@ -1,14 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice , createAsyncThunk} from "@reduxjs/toolkit";
+import authService from "../../Services/authService";
 
 let user=JSON.parse(localStorage.getItem("user"));
 
 let initialState={
     user:user?user:null,
-    isLoading:false,
+    isLoading:true,
     isSuccess:false,
     isError:false,
     message:''
 }
+
+export const register=createAsyncThunk("auth/register",async()=>{
+let data=authService.register({name:"bilal",email:"bilalmalik42011@gmail.com"})
+return data;
+});
+
 
 let authSlice=createSlice({
 name:"auth",
@@ -20,12 +27,31 @@ reducers:{
     state.isError=false;
     state.message='';
     }
+},
+extraReducers(builder){
+builder
+.addCase(register.fulfilled,(state,action)=>{
+    console.log("registeration fulfiled");
+    state.isLoading=false;
+    state.user=action.payload;
+    state.isSuccess=true;
+})
+.addCase(register.rejected,(state,action)=>{
+    console.log("registeration rejected");
+    state.isLoading=false;
+    state.isError=true;
+    state.message="response not found 404"
+})
+.addCase(register.pending,(state)=>{
+state.isLoading=true;
+})
+
 }
 
 
 });
 
 
-export const {reset}=authSlice.actions;
+export let {reset}=authSlice.actions;
 
 export default authSlice.reducer;
